de09d0fe289420d12a9b38405cd29bef7b2fb631
VasaAki <riceRebel@gmail.com>
2022-06-10 11:01:54 -0700

    add subtraction, multiplication, and division as well as display results

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,21 @@ function add(number1, number2) {
   return number1 + number2;
 }
 
+function subtract(number1,number2) {
+  return number1 - number2;
+}
+
+function multiply(number1,number2) {
+  return number1*number2;
+}
+
+function divide(number1,number2) {
+  return number1/number2;
+}
+
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter a second number:"));
-window.alert(add(number1,number2));
+window.alert(number1 + "+" + number2 + "=" + add(number1,number2) +
+  ".\n The subtraction of your numbers equals: " + subtract(number1,number2) + 
+  ".\n The multiplication of your numbers equal: " + multiply(number1,number2) + 
+  ".\n The division of your number equals: " + divide(number1,number2));
